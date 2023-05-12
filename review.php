d6853b7c94a1cfa37f3c46d674224f8f2d293ea9
ravendemor <ravendemor@gmail.com>
2023-05-12 13:49:51 +0400

    [master] - review file

---
+++ review.php
@@ -0,0 +1,23 @@
+function searchByEmail( $email = '' )
+{
+global $limit;
+
+$out = array();
+
+$query = 'SELECT * FROM users WHERE email LIKE ' . $email;
+
+if( $res = @mysql_query( $query ) )
+{
+while( $arr = @mysql_fetch_array( $res ) )
+{
+$out[] = $arr;
+
+if( count( $out ) > $limit )
+{
+break;
+}
+}
+}
+
+return $out;
+}
